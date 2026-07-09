98c291700d7f59e7802be8df89aa6733a30d34ea
OsamaImtiaz-Napollo <osamaimtiaz1401@gmail.com>
2026-07-09 17:30:48 +0500

    Update form validation view to retain user input on error. Added `old()` function to input fields for name, email, and password to enhance user experience during validation failures.

--- resources/views/validation/form-validation.blade.php
+++ resources/views/validation/form-validation.blade.php
@@ -29,14 +29,15 @@
     @csrf
     <div>
         <label for="name">Name</label>
-        <input type="text" name="name" id="name"/>
+        <!-- old('name') is used to display the old value of the name field if there is any error -->
+        <input type="text" name="name" id="name" value="{{ old('name') }}" />
         @error('name')
         <p style="color: red;">{{ $message }}</p>
         @enderror
     </div>
     <div>
         <label for="email">Email</label>
-        <input type="email" name="email" id="email"/>
+        <input type="email" name="email" id="email" value="{{ old('email') }}" />
         <!-- this will display the error message for the email field -->
         @error('email')
         <p style="color: red;">{{ $message }}</p>
@@ -44,7 +45,7 @@
     </div>
     <div>
         <label for="password">Password</label>
-        <input type="password" name="password" id="password"/>
+        <input type="password" name="password" id="password" value="{{ old('password') }}" />
         @error('password')
         <p style="color: red;">{{ $message }}</p>
         @enderror
