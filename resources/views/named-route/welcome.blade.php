5a3f2f8dc5fb6e97721c4318c39d717b30eb82c5
OsamaImtiaz-Napollo <osamaimtiaz1401@gmail.com>
2026-07-13 16:51:06 +0500

    Update comment in welcome.blade.php to clarify the usage of the route() helper function for the 'pf' route. This enhances code readability and understanding of the route's purpose.

--- resources/views/named-route/welcome.blade.php
+++ resources/views/named-route/welcome.blade.php
@@ -1,3 +1,11 @@
 <h2>Welcome page</h2>
-<!-- Example of href using route() helper function -->
+<!-- 
+    This is an example of using Laravel's route() helper in a Blade template to generate a URL for a named route.
+    In the <a> tag below, the href attribute is set dynamically by calling route('pf'), where 'pf' is the name
+    assigned to the route in your routes/web.php file using ->name('pf').
+
+    The {{ }} Blade syntax will evaluate the PHP expression inside and print the result.
+    This means that if the URL for the route ever changes in your routes file, you only need to update it there;
+    all links using route('pf') will automatically point to the new URL, making your code more maintainable.
+-->
 <a href="{{ route('pf') }}">user details page</a>
